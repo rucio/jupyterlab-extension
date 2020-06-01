5e0d4db0ae3517947757f5d62daa64ebaa473814
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-06-01 15:58:32 +0700

    Add stub IPython kernel extension

---
+++ rucio_jupyterlab/kernels/ipython/__init__.py
@@ -0,0 +1,8 @@
+def load_ipython_extension(ipython):
+    ipython.push({ 'did': resolve_did })
+
+def unload_ipython_extension(ipython):
+    pass
+
+def resolve_did(did):
+    return '/eos/rucio/' + did
