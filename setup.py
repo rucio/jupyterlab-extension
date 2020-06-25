a833c5961a4721c0d56db9eba06a142db8098b26
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-06-25 13:45:34 +0700

    Fix pip install error

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ lab_path = os.path.join(HERE, name, "labextension")
 
 # Representative files that should exist after a successful build
 jstargets = [
-    os.path.join(HERE, "lib", "index.js"),
+    os.path.join(HERE, "lib", "src", "index.js"),
 ]
 
 package_data_spec = {
