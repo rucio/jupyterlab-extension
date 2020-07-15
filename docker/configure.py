6596ca970a2929d1bd3ead8f06566ae9e536aca3
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-07-15 20:55:06 +0700

    Omit config on None

--- docker/configure.py
+++ docker/configure.py
@@ -3,6 +3,7 @@ import json
 
 HOME = '/home/jovyan'
 
+
 def write_jupyterlab_config():
     file_path = HOME + '/.jupyter/jupyter_notebook_config.json'
     if not os.path.isfile(file_path):
@@ -17,20 +18,23 @@ def write_jupyterlab_config():
     except:
         config_json = {}
 
-    config_json['RucioConfig'] = {
-        'instances': [
-            {
-                "name": os.getenv('RUCIO_NAME', "default"),
-                "display_name": os.getenv('RUCIO_DISPLAY_NAME', "Default Instance"),
-                "rucio_base_url": os.getenv('RUCIO_BASE_URL'),
-                "destination_rse": os.getenv('RUCIO_DESTINATION_RSE'),
-                "rse_mount_path": os.getenv('RUCIO_RSE_MOUNT_PATH'),
-                "path_begins_at": int(os.getenv('RUCIO_PATH_BEGINS_AT', '0')),
-                "mode": os.getenv('RUCIO_MODE', 'replica')
-            }
-        ]
+    instance_config = {
+        "name": os.getenv('RUCIO_NAME', "default"),
+        "display_name": os.getenv('RUCIO_DISPLAY_NAME', "Default Instance"),
+        "rucio_base_url": os.getenv('RUCIO_BASE_URL'),
+        "rucio_auth_url": os.getenv('RUCIO_AUTH_URL'),
+        "rucio_ca_cert": os.getenv('RUCIO_CA_CERT'),
+        "destination_rse": os.getenv('RUCIO_DESTINATION_RSE'),
+        "rse_mount_path": os.getenv('RUCIO_RSE_MOUNT_PATH'),
+        "path_begins_at": int(os.getenv('RUCIO_PATH_BEGINS_AT', '0')),
+        "mode": os.getenv('RUCIO_MODE', 'replica')
     }
-    
+
+    instance_config = {k: v for k, v in instance_config.items() if v is not None}
+    config_json['RucioConfig'] = {
+        'instances': [instance_config]
+    }
+
     config_file = open(file_path, 'w')
     config_file.write(json.dumps(config_json, indent=2))
     config_file.close()
@@ -54,7 +58,7 @@ def write_ipython_config():
 
     if 'IPKernelApp' not in config_json:
         config_json['IPKernelApp'] = {}
-    
+
     ipkernel_app = config_json['IPKernelApp']
 
     if 'extensions' not in ipkernel_app:
@@ -62,7 +66,7 @@ def write_ipython_config():
 
     if extension_module not in ipkernel_app['extensions']:
         ipkernel_app['extensions'].append(extension_module)
-    
+
     config_file = open(file_path, 'w')
     config_file.write(json.dumps(config_json, indent=2))
     config_file.close()
