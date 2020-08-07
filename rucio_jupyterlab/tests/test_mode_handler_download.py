c7f601c72e31325050dc748b2eb58a8d1c555a08
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-08-07 20:14:18 +0700

    Add tests for pid checking

--- rucio_jupyterlab/tests/test_mode_handler_download.py
+++ rucio_jupyterlab/tests/test_mode_handler_download.py
@@ -20,13 +20,69 @@ def test_did_downloader_is_downloading__lockfile_exists__pid_not_exists__should_
         mock_file.assert_called_with(os.path.join('/path', '.lockfile'), 'r')
 
 
-def test_did_downloader_is_downloading__lockfile_exists__pid_exists__should_return_true(mocker):
+def test_did_downloader_is_downloading__lockfile_exists__pid_exists__process_not_running__should_return_false(mocker):
     mocker.patch.object(os.path, 'isfile', return_value=True)
     mocker.patch.object(psutil, 'pid_exists', return_value=True)
+
+    class MockProcess:
+        def __init__(self, pid):
+            pass
+
+        def is_running(self):   # pylint: disable=no-self-use
+            return False
+
+        def status(self):   # pylint: disable=no-self-use
+            return 'running'
+
+    mocker.patch('rucio_jupyterlab.mode_handlers.download.psutil.Process', MockProcess)
+
+    with patch("builtins.open", mock_open(read_data="123")) as mock_file:
+        result = DIDDownloader.is_downloading('/path')
+        assert not result, 'Invalid return value'
+        mock_file.assert_called_with(os.path.join('/path', '.lockfile'), 'r')
+
+
+def test_did_downloader_is_downloading__lockfile_exists__pid_exists__process_running__status_running__should_return_true(mocker):
+    mocker.patch.object(os.path, 'isfile', return_value=True)
+    mocker.patch.object(psutil, 'pid_exists', return_value=True)
+
+    class MockProcess:
+        def __init__(self, pid):
+            pass
+
+        def is_running(self):   # pylint: disable=no-self-use
+            return True
+
+        def status(self):   # pylint: disable=no-self-use
+            return 'running'
+
+    mocker.patch('rucio_jupyterlab.mode_handlers.download.psutil.Process', MockProcess)
 
     with patch("builtins.open", mock_open(read_data="123")) as mock_file:
         result = DIDDownloader.is_downloading('/path')
         assert result, 'Invalid return value'
+        mock_file.assert_called_with(os.path.join('/path', '.lockfile'), 'r')
+
+
+def test_did_downloader_is_downloading__lockfile_exists__pid_exists__process_running__status_zombie__should_return_false(mocker):
+    mocker.patch.object(os.path, 'isfile', return_value=True)
+    mocker.patch.object(psutil, 'pid_exists', return_value=True)
+
+    class MockProcess:
+        def __init__(self, pid):
+            pass
+
+        def is_running(self):   # pylint: disable=no-self-use
+            return True
+
+        def status(self):   # pylint: disable=no-self-use
+            return 'zombie'
+
+    mocker.patch('rucio_jupyterlab.mode_handlers.download.psutil.Process', MockProcess)
+
+    with patch("builtins.open", mock_open(read_data="123")) as mock_file:
+        result = DIDDownloader.is_downloading('/path')
+        assert not result, 'Invalid return value'
         mock_file.assert_called_with(os.path.join('/path', '.lockfile'), 'r')
 
 
