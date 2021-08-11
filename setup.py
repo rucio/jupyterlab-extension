3fc5c86256fc08650649d5dce277104e6450af99
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-08-11 10:18:01 +0700

    Upgrade rucio-clients to 1.26 lightweight

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup_args = dict(
         "peewee~=3.14.0",
         "jsonschema~=3.2.0",
         "psutil~=5.8.0",
-        "rucio-clients~=1.0"
+        "rucio-clients~=1.26.0"
     ],
     zip_safe=False,
     include_package_data=True,
