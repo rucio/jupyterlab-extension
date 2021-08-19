94c7b55f48eb37c7640bf9525f641b6229ca4067
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-08-19 16:37:00 +0700

    Add list scopes server endpoint

---
+++ rucio_jupyterlab/handlers/list_scopes.py
@@ -0,0 +1,29 @@
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Muhammad Aditya Hilmy, <mhilmy@hey.com>, 2020
+
+import json
+import tornado
+from rucio_jupyterlab.db import get_db
+from rucio_jupyterlab.rucio.authenticators import RucioAuthenticationException
+import rucio_jupyterlab.utils as utils
+from .base import RucioAPIHandler
+
+
+class ListScopesHandler(RucioAPIHandler):
+    @tornado.web.authenticated
+    def get(self):
+        namespace = self.get_query_argument('namespace')
+        rucio = self.rucio.for_instance(namespace)
+
+        try:
+            scopes = rucio.get_scopes()
+            self.finish(json.dumps(scopes))
+        except RucioAuthenticationException:
+            self.set_status(401)
+            self.finish(json.dumps({'error': 'authentication_error'}))
