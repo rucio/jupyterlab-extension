07bc3475641df9b5a995dea43d05ef3bf2c57f0a
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-08-31 22:30:24 +0700

    Not using template due to problem with Jinja

--- rucio_jupyterlab/server.py
+++ rucio_jupyterlab/server.py
@@ -7,7 +7,6 @@
 # Authors:
 # - Muhammad Aditya Hilmy, <mhilmy@hey.com>, 2020
 
-from os.path import join, dirname
 from .handlers import setup_handlers
 
 
@@ -19,9 +18,6 @@ def load_jupyter_server_extension(lab_app):  # pragma: no cover
     lab_app: jupyterlab.labapp.LabApp
         JupyterLab application instance
     """
-    lab_app.web_app.settings["jinja2_env"].loader.searchpath += [
-        join(dirname(__file__), "templates")
-    ]
     setup_handlers(lab_app.web_app)
     lab_app.log.info("Registered Rucio JupyterLab extension at URL path /rucio-jupyterlab")
 
