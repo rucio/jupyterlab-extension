e286cf82a92465dbdb388b430a5467eaf07bf26c
avendrel <albavemo11@gmail.com>
2022-05-19 10:17:19 +0200

    Fix did_type

--- rucio_jupyterlab/handlers/did_search.py
+++ rucio_jupyterlab/handlers/did_search.py
@@ -39,7 +39,7 @@ class DIDSearchHandlerImpl:
             return {
                 'did': entry.get('scope') + ':' + entry.get('name'),
                 'size': entry.get('bytes'),
-                'type': entry.get('did_type').lower()
+                'type': (entry.get('did_type').lower()).split('.')[1]
             }
 
         result = utils.map(dids, mapper)
