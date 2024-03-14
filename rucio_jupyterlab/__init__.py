cea8661e816984c40e0a2edd054fe1876f55277c
Giovanni Guerrieri <toshiro.yamasuke@gmail.com>
2024-03-14 21:28:44 +0100

    fixing path

--- rucio_jupyterlab/__init__.py
+++ rucio_jupyterlab/__init__.py
@@ -6,7 +6,7 @@ HERE = os.path.abspath(os.curdir)
 
 # HERE = Path(__file__).parent.parent
 
-with (HERE / "jupyterlab-extension" / "rucio_jupyterlab" / "labextension" / "package.json").open() as fid:
+with (HERE / "rucio_jupyterlab" / "labextension" / "package.json").open() as fid:
     data = json.load(fid)
 
 
