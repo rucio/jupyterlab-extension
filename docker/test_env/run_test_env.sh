82a6c2d121bba299b5cd41364890a817f3cc90c1
Francesc Torradeflot <torradeflot@pic.es>
2025-03-20 08:40:56 +0100

    Modify the test env to pick the docker image version from the rucio repository

--- docker/test_env/run_test_env.sh
+++ docker/test_env/run_test_env.sh
@@ -11,6 +11,29 @@ else
 	RUN_TEST=0
 fi
 
+# Get the tag of the rucio-dev image corresponding to the rucio repository version
+# Path to your Rucio repository
+SCRIPT_DIR=$(dirname "$0")
+REPO_PATH=$(realpath "$SCRIPT_DIR/rucio")
+
+# Change directory to the Git repository
+if cd "$REPO_PATH"; then
+    # Get the Git tag
+    RUCIO_TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+
+    if [ -n "$RUCIO_TAG" ]; then
+        export RUCIO_IMG="release-$RUCIO_TAG"
+    else
+        export RUCIO_IMG="latest-alma9"
+    fi
+
+    # Change back to the original directory (optional)
+    cd - >/dev/null 2>&1
+else
+  echo "Error: Could not access Git repository at $REPO_PATH."
+fi
+
+
 # Run Docker Compose with the chosen file and profile
 docker compose \
 	--file rucio/etc/docker/dev/docker-compose.yml \
@@ -24,5 +47,5 @@ docker compose \
 # WARNING: if the test cluster wasn't properly closed this step will crash
 if [ "$RUN_TEST" -eq "1" ]
 then
-	docker exec -it dev-rucio-1 /bin/bash -c "tools/run_tests.sh -ir"
+	docker exec -it dev_rucio_1 /bin/bash -c "tools/run_tests.sh -ir"
 fi
