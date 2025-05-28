ad51abfce1e09da3ff146cc27844b53376c159e0
Enrique Garcia <40355845+garciagenrique@users.noreply.github.com>
2025-05-28 14:54:12 +0200

    fix: complete migration to Jupyter server (#75)

--- rucio_jupyterlab/__init__.py
+++ rucio_jupyterlab/__init__.py
@@ -5,7 +5,7 @@ except ImportError:
     # in editable mode with pip. It is highly recommended to install
     # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
     import warnings
-    warnings.warn("Importing 'engarcia_py_package_name' outside a proper installation.")
+    warnings.warn("Importing 'rucio_jupyterlab' outside a proper installation.")
     __version__ = "dev"
 
 def _jupyter_labextension_paths():
@@ -15,7 +15,7 @@ def _jupyter_labextension_paths():
     }]
 
 
-def _jupyter_server_extension_paths():  # pragma: no cover
+def _jupyter_server_extension_points():  # pragma: no cover
     return [{
         "module": "rucio_jupyterlab.server"
     }]
